a046e4a413980e515d6fa458546a3a13d33eea04
Abdeljabbar Elassali <115737234+asly-97@users.noreply.github.com>
2024-07-25 01:08:02 -0400

    initial commit

---
+++ src/main/java/com/revature/p1demo/P1demoApplication.java
@@ -0,0 +1,13 @@
+package com.revature.p1demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1demoApplication.class, args);
+	}
+
+}
